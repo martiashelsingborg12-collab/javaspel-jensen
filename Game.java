73b3538ebcc8979dbf3fd3366428594a5f7c6911
Haider Abdul Abbas <martias.helsingborg12@gmail.com>
2025-09-26 23:22:43 +0200

    ladde till spel

--- Game.java
+++ Game.java
@@ -4,26 +4,36 @@ public class Game{
     public void run(){
         System.out.println("välkomen till spelet: ");
 	Scanner scanner = new Scanner(System.in);
+	Player player1 = addPlayer(scanner);
+	Player player2 = addPlayer(scanner);
 
-	System.out.print("vad är ditt förnamn: ");
-	String firstName = scanner.nextLine();
-
-	System.out.print("vad är ditt efternamn: ");
-	String lastName = scanner.nextLine();
-
-
-        while(true){
-
-        System.out.println("hej: " + firstName + " " + lastName);
-        System.out.println("quit avsluta spelet, play köra spelet: ");
-	String command = scanner.nextLine();
-
-	if("quit".equals(command)){
-            break;
-	}
-		                                                     
+        while(true){ 
+            System.out.println("hej spelare");
+            System.out.println("quit avsluta spelet, play köra spelet: ");
+       	    String command = scanner.nextLine();
+ 
+	    if("quit".equals(command)){
+                break;
+	    }
+	    if("play".equals(command)){
+                play( player1, player2);
+            }
 	}
 	
 		                                 
     }
-}
+
+    private Player addPlayer(Scanner scanner){
+          System.out.print("vad är ditt förnamn: ");
+          String firstName = scanner.nextLine();
+          
+          System.out.print("vad är ditt efternamn: ");
+          String lastName = scanner.nextLine();
+          return new Player(firstName, lastName);
+    }
+   
+    private void play( Player player1, Player player2){
+        System.out.println(player1.getName());
+        System.out.println(player2.getName());
+    }
+} 
